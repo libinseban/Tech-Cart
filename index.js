31070af4b9c2f5a84a2f37eafa940feb1b31a682
Libin Seban <libinseban97@gmail.com>
2025-02-07 19:00:03 +0530

    backend update

--- index.js
+++ index.js
@@ -12,13 +12,19 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-  origin: ["https://frondend-alpha.vercel.app", "http://localhost:5173"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  exposedHeaders: ["set-cookie"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    exposedHeaders: ["set-cookie"],
 };
-
+  
 app.use(cors(corsOptions))
 ;
 
