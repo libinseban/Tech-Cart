28edae9dec40be513cb2c83174b43497c65a5c2f
Libin Seban <libinseban97@gmail.com>
2025-02-11 01:35:46 +0530

    updated backdn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const corsOptions = {
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, origin);
       } else {
-        callback(new Error("Not allowed by CORS"));
-      }
+        callback(null, false); 
+    }
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
