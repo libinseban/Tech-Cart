01c0d72cb4d8ed43781d20d6d0eff0e8348d0f97
Libin Seban <libinseban97@gmail.com>
2025-04-09 11:56:28 +0530

    backend updated

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const app = express();
 
 
 const allowedOrigins = [
-    "http://localhost:5173", 
+    "http://localhost:5173",
+    "http://localhost:5174", 
     "http://localhost:5175", 
     "https://frontend-chi-ashy-91.vercel.app",
-    "https://libinseban.github.io/TechCart-Frontend/"
 ];
 
 // CORS options
